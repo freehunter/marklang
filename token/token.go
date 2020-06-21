65428200efd76dcac3d9840859d714c4d5e6bde5
Mark Hunt <thefreehunter@gmail.com>
2020-06-21 12:52:18 -0400

    added more keywords

--- token/token.go
+++ token/token.go
@@ -36,13 +36,23 @@ const (
 	 RBRACE = "}"
 
 	 // Keywords
-	 FUNCTION = "FUNCTION"
-	 LET 	  = "LET"
+	 FUNCTION 	= "FUNCTION"
+	 LET		= "LET"
+	 TRUE		= "TRUE"
+	 FALSE		= "FALSE"
+	 IF 		= "IF"
+	 ELSE		= "ELSE"
+	 RETURN		= "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn": FUNCTION,
-	"let": LET,
+	"fn": 		FUNCTION,
+	"let": 		LET,
+	"true": 	TRUE,
+	"false": 	FALSE,
+	"if":		IF,
+	"else":		ELSE,
+	"return":	RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
