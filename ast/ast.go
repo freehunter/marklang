50bad4eb713ed779d02a3b94632341e2dcfc4edf
Mark Hunt <thefreehunter@gmail.com>
2020-06-21 16:29:51 -0400

    extending parser, added error handling

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,24 @@
 package ast
 
+import "marklang/token"
+
+type LetStatement struct {
+	Token token.Token //the token.LET token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()		{}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
+
+type Identifier struct {
+	Token token.Token //the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()		{}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
 type Node interface {
 	TokenLiteral() string
 }
